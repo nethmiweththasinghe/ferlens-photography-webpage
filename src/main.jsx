2562ee99e35fbbf8d8b53b62fc7c97c6f26ea5ef
Nethmi Weththasinghe <95464349+nethmiweththasinghe@users.noreply.github.com>
2026-05-10 18:28:51 +1200

    react-snap revert

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot, hydrateRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import { StrictMode } from 'react'
+  import { createRoot } from 'react-dom/client'
+  import './index.css'
+  import App from './App.jsx'
 
-const rootElement = document.getElementById("root");
-
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(
-    rootElement,
+  createRoot(document.getElementById('root')).render(
     <StrictMode>
       <App />
     </StrictMode>,
-  );
-} else {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-}
+  )
